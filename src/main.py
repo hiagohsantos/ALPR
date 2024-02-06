661b1d122a1e8c924138fe9d8af23051679e2703
hiagohsantos <hiago1996@outlook.com>
2024-02-06 19:36:23 -0300

    fix(desktop): adjuste the projet structure

--- src/main.py
+++ src/main.py
@@ -24,119 +24,140 @@ class ALPRapp:
         self.startTimeRec = int(time.time())
         self.progress = 0
 
-    def tk_image(self, imagem, x, y):
-        imagem = Image.fromarray(imagem)
-        return ctk.CTkImage(light_image=imagem, dark_image=imagem, size=(x, y))
+    def tk_image(self, image, x, y):
+        image = Image.fromarray(image)
+        return ctk.CTkImage(light_image=image, dark_image=image, size=(x, y))
 
-    def create_widgets(self):
-        # Root Frames
-        self.cam_frame = ctk.CTkFrame(
-            self.root, width=660, height=550, fg_color="#343436"
-        )
-        self.cam_frame.place(in_=self.root, y=20, x=20)
-
-        self.menu_frame = ctk.CTkFrame(
-            self.root, width=200, height=550, fg_color="#343436"
-        )
-        self.menu_frame.place(in_=self.root, x=690, y=20)
-
-        self.botton_frame = ctk.CTkFrame(
-            self.root, width=870, height=200, fg_color="#343436"
-        )
-        self.botton_frame.place(in_=self.root, x=20, y=580)
-
-        # Start Button
-        self.start_button = ctk.CTkButton(
-            self.menu_frame,
-            text="Iniciar",
-            width=180,
-            height=50,
-            command=lambda: self.capture_timer(),
-            font=ctk.CTkFont(size=20),
-        )
-        self.start_button.place(in_=self.menu_frame, x=10, y=20)
-
-        # Cam widgets
-        self.title_cam = ctk.CTkLabel(
-            self.cam_frame, text="Câmera", font=ctk.CTkFont(size=20)
-        ).place(in_=self.cam_frame, relx=0.45, y=10)
-
-        self.videoCam = ctk.CTkLabel(
-            self.cam_frame, text="", width=640, height=480, fg_color="#3c3c3d"
-        )
-        self.videoCam.place(in_=self.cam_frame, x=10, y=50)
-
-        self.switch_variable = ctk.StringVar(value="off")
-        self.cam_switch = ctk.CTkSwitch(
-            self.cam_frame,
-            text="",
-            switch_width=50,
-            switch_height=25,
-            variable=self.switch_variable,
-            onvalue="on",
-            offvalue="off",
-        )
-        self.cam_switch.place(
-            in_=self.cam_frame,
-            x=600,
-            y=10,
-        )
-
-        # Menu Widgets
-        self.progressbar = ctk.CTkProgressBar(
-            self.menu_frame,
-            orientation="horizontal",
-            width=180,
-            mode="determinate",
-            determinate_speed=1,
-        )
-        self.progressbar.set(self.progress)
-        self.progressbar.place(
-            in_=self.menu_frame,
-            x=10,
-            y=100,
-        )
-
-        coord_label = ctk.CTkLabel(
-            self.menu_frame, text="Coordenadas", font=ctk.CTkFont(size=12)
-        ).place(in_=self.menu_frame, x=10, y=125)
-
-        self.textStatus = ctk.CTkLabel(
-            self.menu_frame, text="", fg_color="#3c3c3d", width=180, height=30
-        )
-        self.textStatus.place(in_=self.menu_frame, x=10, y=150)
-
-        # Model Image
-        model_plate = Image.fromarray(
-            cv2.resize(
-                cv2.cvtColor(
-                    cv2.imread("../images/modelPlate.jpeg"), cv2.COLOR_BGR2RGBA
-                ),
-                (150, 50),
-                interpolation=cv2.INTER_AREA,
+    def create_frames(self) -> None:
+        try:
+            # Root Frames
+            self.cam_frame = ctk.CTkFrame(
+                self.root, width=660, height=550, fg_color="#343436"
             )
-        )
-        model_plate = ctk.CTkImage(
-            light_image=model_plate, dark_image=model_plate, size=(150, 50)
-        )
-        # Segmented Plate Label
-        ctk.CTkLabel(
-            self.menu_frame, text="Imagem Segmentada", font=ctk.CTkFont(size=12)
-        ).place(in_=self.menu_frame, x=10, y=180)
-        self.segmented_plate = ctk.CTkLabel(self.menu_frame, image=model_plate, text="")
-        self.segmented_plate.place(in_=self.menu_frame, x=25, y=210)
+            self.cam_frame.place(in_=self.root, y=20, x=20)
 
-        ctk.CTkLabel(
-            self.menu_frame, text="Imagem Limiarizada", font=ctk.CTkFont(size=12)
-        ).place(in_=self.menu_frame, x=10, y=270)
-        self.thresholded_plate = ctk.CTkLabel(self.menu_frame, image=model_plate, text="")
-        self.thresholded_plate.place(in_=self.menu_frame, x=25, y=300)
+            self.menu_frame = ctk.CTkFrame(
+                self.root, width=200, height=550, fg_color="#343436"
+            )
+            self.menu_frame.place(in_=self.root, x=690, y=20)
 
-        ctk.CTkLabel(
-            self.menu_frame, text="Imagem Reorientada", font=ctk.CTkFont(size=12)
-        ).place(in_=self.menu_frame, x=10, y=360)
-        self.reoriented_plate = ctk.CTkLabel(self.menu_frame, image=model_plate, text="")
-        self.reoriented_plate.place(in_=self.menu_frame, x=25, y=390)
+            self.botton_frame = ctk.CTkFrame(
+                self.root, width=870, height=200, fg_color="#343436"
+            )
+            self.botton_frame.place(in_=self.root, x=20, y=580)
+        except Exception as e:
+            print(f"Houve um problema ao criar os Frames -> {e}")
+            raise e
+
+    def create_cam_components(self) -> None:
+        try:
+            # Cam widgets
+            self.title_cam = ctk.CTkLabel(
+                self.cam_frame, text="Câmera", font=ctk.CTkFont(size=20)
+            ).place(in_=self.cam_frame, relx=0.45, y=10)
+
+            self.videoCam = ctk.CTkLabel(
+                self.cam_frame, text="", width=640, height=480, fg_color="#3c3c3d"
+            )
+            self.videoCam.place(in_=self.cam_frame, x=10, y=50)
+
+            self.switch_variable = ctk.StringVar(value="off")
+            self.cam_switch = ctk.CTkSwitch(
+                self.cam_frame,
+                text="",
+                switch_width=50,
+                switch_height=25,
+                variable=self.switch_variable,
+                onvalue="on",
+                offvalue="off",
+            )
+            self.cam_switch.place(
+                in_=self.cam_frame,
+                x=600,
+                y=10,
+            )
+        except Exception as e:
+            print(f"Houve um problema ao criar os componentes da camera -> {e}")
+            raise e
+
+    def create_menu_components(self) -> None:
+        try:
+            # Start Button
+            self.start_button = ctk.CTkButton(
+                self.menu_frame,
+                text="Iniciar",
+                width=180,
+                height=50,
+                command=lambda: self.capture_timer(),
+                font=ctk.CTkFont(size=20),
+            )
+            self.start_button.place(in_=self.menu_frame, x=10, y=20)
+
+            # Menu Widgets
+            self.progressbar = ctk.CTkProgressBar(
+                self.menu_frame,
+                orientation="horizontal",
+                width=180,
+                mode="determinate",
+                determinate_speed=1,
+            )
+            self.progressbar.set(self.progress)
+            self.progressbar.place(
+                in_=self.menu_frame,
+                x=10,
+                y=100,
+            )
+
+            coord_label = ctk.CTkLabel(
+                self.menu_frame, text="Coordenadas", font=ctk.CTkFont(size=12)
+            ).place(in_=self.menu_frame, x=10, y=125)
+
+            self.textStatus = ctk.CTkLabel(
+                self.menu_frame, text="", fg_color="#3c3c3d", width=180, height=30
+            )
+            self.textStatus.place(in_=self.menu_frame, x=10, y=150)
+
+            # Model Image
+            model_plate = Image.fromarray(
+                cv2.resize(
+                    cv2.cvtColor(
+                        cv2.imread("../images/modelPlate.jpeg"), cv2.COLOR_BGR2RGBA
+                    ),
+                    (150, 50),
+                    interpolation=cv2.INTER_AREA,
+                )
+            )
+            model_plate = ctk.CTkImage(
+                light_image=model_plate, dark_image=model_plate, size=(150, 50)
+            )
+            # Segmented Plate Label
+            ctk.CTkLabel(
+                self.menu_frame, text="Imagem Segmentada", font=ctk.CTkFont(size=12)
+            ).place(in_=self.menu_frame, x=10, y=180)
+            self.segmented_plate = ctk.CTkLabel(
+                self.menu_frame, image=model_plate, text=""
+            )
+            self.segmented_plate.place(in_=self.menu_frame, x=25, y=210)
+
+            ctk.CTkLabel(
+                self.menu_frame, text="Imagem Limiarizada", font=ctk.CTkFont(size=12)
+            ).place(in_=self.menu_frame, x=10, y=270)
+            self.thresholded_plate = ctk.CTkLabel(
+                self.menu_frame, image=model_plate, text=""
+            )
+            self.thresholded_plate.place(in_=self.menu_frame, x=25, y=300)
+
+            ctk.CTkLabel(
+                self.menu_frame, text="Imagem Reorientada", font=ctk.CTkFont(size=12)
+            ).place(in_=self.menu_frame, x=10, y=360)
+            self.reoriented_plate = ctk.CTkLabel(
+                self.menu_frame, image=model_plate, text=""
+            )
+            self.reoriented_plate.place(in_=self.menu_frame, x=25, y=390)
+
+        except Exception as e:
+            print(f"Houve um problema ao criar os componentes do menu -> {e}")
+            raise e
 
         # def processa_imagem(imagem):
         #     # Converta a imagem para escala de cinza
