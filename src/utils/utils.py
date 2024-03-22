98705d730a45a78e12825ac49a804e562e1c6755
Hiago <hiago1996@outlook.com>
2024-03-22 09:00:15 -0300

    feat(rasp): add new models

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -26,7 +26,7 @@ _TEXT_COLOR = (0, 255, 0)  # verde
 
 # Initialize the object detection model
 base_options = core.BaseOptions(
-    file_name="../Models/modelo_EL0_AP53.tflite", use_coral=False, num_threads=4
+    file_name="../Models/eflite0_AP6634.tflite", use_coral=False, num_threads=4
 )
 detection_options = processor.DetectionOptions(max_results=1, score_threshold=0.3)
 options = vision.ObjectDetectorOptions(
